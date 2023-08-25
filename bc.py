backpropagation fixed on barrierNN

bc.py
@@ -33,7 +33,7 @@ parser.add_argument(
     choices=SAFETY_GYM_ENVS,
     help='Safety_GYM_Env',
 )
-parser.add_argument('--eta', type=float, default=0.1, help='Eta on safety parameter')
+parser.add_argument('--eta', type=float, default=10, help='Eta on safety parameter')
 parser.add_argument('--epsilon', type=float, default=0.01, help='Margin used to find bc')
 parser.add_argument('--observation_type', default='rgb_image')
 parser.add_argument('--symbolic-env', action='store_true', help='Symbolic features')
@@ -247,35 +247,42 @@ param_list = (
     + list(encoder.parameters())
 )
 value_barrier_controller_param_list = list(value_model.parameters()) + list(barrier_model.parameters()) + list(controller.parameters())
+
 cbf_params_list = (
     list(barrier_model.parameters()) 
     + list(controller.parameters())
 )
 params_list = param_list + value_barrier_controller_param_list
 print("transition, observation, reward, encoder, barrier, controller, value models are ready")
+
 model_optimizer = optim.Adam(
     param_list, lr=0 if args.learning_rate_schedule != 0 else args.model_learning_rate, eps=args.adam_epsilon
 )
+
 barrier_optimizer = optim.Adam(
     barrier_model.parameters(),
     lr=0 if args.learning_rate_schedule != 0 else args.barrier_learning_rate,
     eps=args.adam_epsilon,
 )
+
 controller_optimizer = optim.Adam(
     barrier_model.parameters(),
     lr=0 if args.learning_rate_schedule != 0 else args.controller_learning_rate,
     eps=args.adam_epsilon,
 )
+
 cbf_optimizer = optim.Adam(
     cbf_params_list,
     lr=0 if args.learning_rate_schedule != 0 else args.controller_learning_rate,
     eps=args.adam_epsilon,
 )
+
 value_optimizer = optim.Adam(
     value_model.parameters(),
     lr=0 if args.learning_rate_schedule != 0 else args.value_learning_rate,
     eps=args.adam_epsilon,
 )
+
 if args.models != '' and os.path.exists(args.models):
     print("loading pre-trained models")
     model_dicts = torch.load(args.models)
@@ -547,15 +554,15 @@ for episode in tqdm(range(metrics['episodes'][-1] + 1, args.episodes + 1), total
             imagination_traj = imagine_ahead(
                 bc_states, bc_beliefs, controller, transition_model, args.planning_horizon
             )
+            
         imged_beliefs, imged_prior_states, imged_prior_means, imged_prior_std_devs = imagination_traj
 
         
-        # Calculate the Barrier loss and update the barrier_model
+        # Calculate the Barrier loss and Controller loss and update the barrier_model and controller
         # Retrieve imageined safety costs pred
-        with FreezeParameters(model_modules + cbf_modules):
+        with FreezeParameters(model_modules):
             imged_cost = bottle(cost_model, (imged_beliefs, imged_prior_states))
-            imged_barrier = bottle(barrier_model, (imged_beliefs, imged_prior_states))
-        
+                
         with FreezeParameters(model_modules + value_model.modules):
             imged_reward = bottle(reward_model, (imged_beliefs, imged_prior_states))
             value_pred = bottle(value_model, (imged_beliefs, imged_prior_states))
@@ -565,28 +572,19 @@ for episode in tqdm(range(metrics['episodes'][-1] + 1, args.episodes + 1), total
         )
 
         controller_return = - torch.mean(torch.sum(returns, dim=0))     
+                
+        imged_barrier = bottle(barrier_model, (imged_beliefs, imged_prior_states))
+
         barrier_return = barrier_loss_return(imged_cost, imged_barrier, args.cost_threshold, args.epsilon)
-        # print(imged_cost.shape, imged_barrier.shape, torch.sum(barrier_return, dim=0).shape)
         barrier_loss = torch.mean(torch.sum(barrier_return, dim=1))
-        # print(f'barrier_loss grad is: {barrier_loss.requires_grad}\n')
-        print(f'Barrier loss: {barrier_loss}\n')
-        # print(f'Controller loss: {type(controller_loss)}\n')
         controller_loss = args.eta * barrier_loss + controller_return
-        # print(controller_loss.requires_grad)
         cbf_optimizer.zero_grad()
-        # cbf_loss.retain_grad()
         controller_loss.backward()
         nn.utils.clip_grad_norm_(cbf_params_list, args.grad_clip_norm, norm_type=2)
         cbf_optimizer.step()
-        # print(controller.modules[0].weight.grad)
-        # print(f'CBF Loss: {controller_loss}\n')
-        # print(f'Barrier Loss: {barrier_loss}\n')
-
-        # print(barrier_model.modules[0].weight.grad)
-        # Calculate the Controller loss and update the controller 
-        # Retrieve imageined rewards and pred values function
         
-
+        print(controller.modules[0].weight.grad)
+        print(barrier_model.modules[0].weight.grad)
 
 
         # CBF-Dreamer implementation: value loss calculation and optimization

utils/utils.py
@@ -128,10 +128,10 @@ def lambda_return(imged_reward, value_pred, bootstrap, discount=0.99, lambda_=0.
     outputs = list(reversed(outputs))
     outputs = torch.stack(outputs, 0)
     returns = outputs
-    # print(returns.size())
+    print(returns)
     return returns
 
-# Out-dated Barrier Calculation Function 
+# Out-dated Barrier Loss Calculation Function 
 def loss_barrier(imged_cost, barrier_pred, COST_THRESHOLD,  _epsilon, _DT = 0.002):
     safe = True
     losses = []
@@ -168,6 +168,7 @@ def loss_barrier(imged_cost, barrier_pred, COST_THRESHOLD,  _epsilon, _DT = 0.00
     return losses
 
 
+# Tensor Calculation based faster version barrier loss function
 def barrier_loss_return(imged_cost, barrier_prev, COST_THRESHOLD, _epsilon, _DT = 0.02):
     imged_cost = torch.transpose(imged_cost, 0, 1)
     barrier_prev = torch.transpose(barrier_prev, 0, 1)
@@ -186,18 +187,22 @@ def barrier_loss_return(imged_cost, barrier_prev, COST_THRESHOLD, _epsilon, _DT 
     cost_barrier_derivative = F.relu(cost_barrier[1:] - derivative - barrier_prev[1:])
     supp = torch.zeros((1, cost_barrier_derivative.shape[1]), device='cuda')
     cost_barrier_derivative = torch.cat([supp, cost_barrier_derivative])
+    # print(cost_barrier_derivative.grad_fn)
     # Safe state is 0, correctly categorized unsafe state is negative, and wrongly categorized unsafe state is positive
     cost_barrier_unsafe_mask = torch.div(F.relu(barrier_prev * unsafe_mask), barrier_prev)
     cost_barrier_unsafe_mask = cost_barrier_unsafe_mask.bool().int()
     epsilon_unsafe_mask = _epsilon * cost_barrier_unsafe_mask
     cost_barrier_unsafe = cost_barrier_unsafe_mask * barrier_prev - epsilon_unsafe_mask
+    # print(cost_barrier_unsafe.grad_fn)
     # Unsafe state is 0, correctly categorized safe state is positive, and wrongly categorized safe state is negative
     cost_barrier_safe_mask = torch.div(F.relu(- safe_mask * barrier_prev), barrier_prev)
     cost_barrier_safe_mask = cost_barrier_safe_mask.bool().int()
     epsilon_safe_mask = _epsilon * cost_barrier_safe_mask
     cost_barrier_safe = epsilon_safe_mask - cost_barrier_safe_mask * barrier_prev
+    # print(cost_barrier_safe.grad_fn)
     # Ensemble all the cost together
     loss = cost_barrier_derivative + cost_barrier_safe + cost_barrier_unsafe
+    # print(loss)
     return loss
 
 